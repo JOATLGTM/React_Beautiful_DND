4c456529cd07a18d8da79f8532b53f3ec66f4d8b
JOAT <joat@JOATs-iMac-Pro.local>
2020-10-09 22:03:38 -0400

    Updates

---
+++ precache-manifest.6a6e0f3b3bcff7ab73c7ac8c197c2e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d96f038ed9e72f5cc627fb54977d6e",
+    "url": "/React_Beautiful_DND/index.html"
+  },
+  {
+    "revision": "ae27ebb092265ea8cc65",
+    "url": "/React_Beautiful_DND/static/css/main.89d28acf.chunk.css"
+  },
+  {
+    "revision": "37fe0afd366acac43329",
+    "url": "/React_Beautiful_DND/static/js/2.ad68c7a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Beautiful_DND/static/js/2.ad68c7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae27ebb092265ea8cc65",
+    "url": "/React_Beautiful_DND/static/js/main.e050137c.chunk.js"
+  },
+  {
+    "revision": "b78db8451526f0244d35",
+    "url": "/React_Beautiful_DND/static/js/runtime-main.32b23ada.js"
+  }
+]);
